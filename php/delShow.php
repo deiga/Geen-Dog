47134f0e055c7f0ba0c7f13d28e25e2e90fb9bc6
Timo Sand <timo.sand@kapsi.fi>
2010-07-19 23:28:45 +0300

    Added show deletion functionality

---
+++ php/delShow.php
@@ -0,0 +1,12 @@
+<?php 
+  require_once($_SERVER['DOCUMENT_ROOT']."/php/functions.php");
+  initSession();
+  
+  $link = connect();
+  not_connected($link);
+  $db = 'roydonf_roydon';
+  inv_db($db);
+  if (loggedIn() && isset($_POST['del'])) {
+    deleteRow($_REQUEST['del']); 
+  }
+?>
