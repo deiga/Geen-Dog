0e69bee230c544a1c0ee09df3a2fc8717dc5d07f
Timo Sand <timo.sand@kapsi.fi>
2011-03-22 12:30:52 +0200

    Test-output

--- products.php
+++ products.php
@@ -33,7 +33,7 @@
           <h1>roydon</h1>
           <p>Kenneltarvikkeet</p>
         </div>
-        <?php printMenu(0);?>
+        <?php printMenu(0); var_dump($_SERVER);?>
       </div>
       <div id="sivusisalto" class="shadow">
         <section id="content" class="products">
