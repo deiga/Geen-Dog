720d418e80afbfa6167e900e62594d39b13e75e1
Timo Sand <timo.sand@kapsi.fi>
2011-03-24 22:17:51 +0200

    more debug

--- php/functions.php
+++ php/functions.php
@@ -199,7 +199,7 @@
   }
 
   function printYearsShows($year, $db) {
-    echo "<span style='display: none;'>$year</span>";
+    echo "<span style='display: none;'>$db</span>";
       $shows = $db->shows()->where("aika > ?", $year . '-00-00')->where("aika < ?", $year1+1 . '-00-00')->order("aika ASC");
 
     foreach ($shows as $show) {
