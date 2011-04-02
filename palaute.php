e187c560d596f93d061380cc622151292a24c76a
Timo Sand <timo.sand@kapsi.fi>
2011-04-03 02:21:47 +0300

    Adding some JS to the sauce

--- palaute.php
+++ palaute.php
@@ -26,11 +26,57 @@
     <!--[if lte IE 6]>
       <link rel="stylesheet" type="text/css" href="css/roydon_ie5.css" />
     <![endif]-->
-    <script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+    <script type="text/javascript">
+      var messageDelay = 2000;  // How long to display status messages (in milliseconds)
       var RecaptchaOptions = {
         theme : 'white',
         tabindex : 2
       };
+
+      // Init the form once the document is ready
+      $( init );
+
+
+      // Initialize the form
+
+      function init() {
+
+        // Hide the form initially.
+        // Make submitForm() the form's submit handler.
+        // Position the form so it sits in the centre of the browser window.
+        $('#contactForm').hide().submit( submitForm ).addClass( 'positioned' );
+
+        // When the "Send us an email" link is clicked:
+        // 1. Fade the content out
+        // 2. Display the form
+        // 3. Move focus to the first field
+        // 4. Prevent the link being followed
+
+        $('a[href="#contactForm"]').click( function() {
+          $('#content').fadeTo( 'slow', .2 );
+          $('#contactForm').fadeIn( 'slow', function() {
+            $('#senderName').focus();
+          } )
+
+          return false;
+        } );
+
+        // When the "Cancel" button is clicked, close the form
+        $('#cancel').click( function() {
+          $('#contactForm').fadeOut();
+          $('#content').fadeTo( 'slow', 1 );
+        } );
+
+        // When the "Escape" key is pressed, close the form
+        $('#contactForm').keydown( function( event ) {
+          if ( event.which == 27 ) {
+            $('#contactForm').fadeOut();
+            $('#content').fadeTo( 'slow', 1 );
+          }
+        } );
+
+      }
     </script>
   </head>
   <body onload="curpage()">
