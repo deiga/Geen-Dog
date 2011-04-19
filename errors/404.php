e64bfbf0bc003416dbef18fef9ec83af33a87289
Timo Sand <timo.sand@kapsi.fi>
2011-04-19 23:41:30 +0300

    Moved GAnalytics to each page

--- errors/404.php
+++ errors/404.php
@@ -17,6 +17,19 @@
 		<!--[if IE 7]>
 			<link rel="stylesheet" type="text/css" href="http://roydon.fi/css/roydon_ie7.css" />
 		<![endif]-->
+		<script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-22841460-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
 	</head>
 	<body >
 		<div id="takala">
--- nayttelyt.php
+++ nayttelyt.php
@@ -35,7 +35,7 @@
     <![endif]-->
     <script type="text/javascript" src="/js/roydon.js"></script>
     <script type="text/javascript">
-      document.write("<script type='text/javascript'  src='http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js'></script>");
+      document.write("<script type='text/javascript'  src='http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js'></sc"+"ript>");
 
       $(document).ready(function() {
         $('a.delete').click(function(e) {
@@ -50,6 +50,19 @@
           $('pretty-tbl').load('nayttelyt.php','year=' . $('yearLink').text())
         });
       });
+    </script>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-22841460-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
     </script>
   </head>
   <body onload="curpage()">
