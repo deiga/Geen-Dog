03fecf03f97ede69425fb2cea85e867dfc5dbfb2
Timo Sand <timo.sand@kapsi.fi>
2011-04-28 21:14:51 +0300

    This query is weird

--- php/login.php
+++ php/login.php
@@ -16,7 +16,7 @@
 	$pass = $_POST['passwd'];
 
   $users = $roydon->user()
-    ->where("username = ?", "$user")
+    ->where("username = $user")
     ->where("password = MD5($pass)");
   // $db = 'roydonf_roydon';
   // $query = "SELECT * FROM user WHERE username = '$user' AND password = md5('$pass')";
